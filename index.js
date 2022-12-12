d24b5d27e96578ecaef54225ef77a1f8259ce206
fgoncalvesb <40902412+fgoncalvesb@users.noreply.github.com>
2022-12-12 21:30:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
+// Global variables that then will be used as to define the URL for a POST of an API
 var originStationUrl = "";
 var destionationStationUrl = "";
 
+// Function that validates the form
 function validateOriginDestination() {
     
     // I save into variables each part of the form that I want to validate
     let x = document.forms["originDestinationForm"]["originStation"].value;
     let y = document.forms["originDestinationForm"]["destionationStation"].value;
 
-    //console.log(x);
-    //console.log(y);
-    //console.log(isNaN(x));
-
     // Conditions that I want to validate
     if (isNaN(x) === false || isNaN(y) === false) {
 
-    // When conditions are not met, I want the borders of the field to become red, to select the text and focus on the field.
+    // When conditions are not met, I want the borders of the field to become red..
     document.forms.originDestinationForm.originStation.style.borderColor = "red";
     document.forms.originDestinationForm.destionationStation.style.borderColor = "red";
-    //document.forms.insertTaskForm.newTask.select();  
-    //document.forms.insertTaskForm.newTask.focus(); 
 
     // In the "span" part of the label, I want for a message to appear if validation is not passed
     document.getElementById('originDestinationFeedback').innerHTML="<b>*Please insert names and not numbers</b>";
@@ -26,7 +22,8 @@ function validateOriginDestination() {
     // I Return false so there is no submission (no refreshing)
     return false;
     }
-
+ 
+    // If input is valited, we remove the red borders in fields and then define the input as "x" and "y" and execute a function that calls 
     document.forms.originDestinationForm.originStation.style.borderColor = "";
     document.forms.originDestinationForm.destionationStation.style.borderColor = "";
     document.getElementById('originDestinationFeedback').innerHTML="";
@@ -34,17 +31,16 @@ function validateOriginDestination() {
     originStationUrl = x;
     destionationStationUrl = y;
 
-    loadJSONFile();
+    getTimetables();
 
     return false;
 }
 
 
-function loadJSONFile() {
+function getTimetables() {
 
+    // I now use the input as to define the origin station and destionation station
     var url = "https://rata.digitraffic.fi/api/v1/live-trains/station/"+originStationUrl+"/"+destionationStationUrl+"?include_nonstopping=false&limit=1";
-
-    //ml tna
 
     // Create AJAX object
     var xmlhttp = new XMLHttpRequest();
@@ -56,25 +52,26 @@ function loadJSONFile() {
     xmlhttp.onreadystatechange = function() {
             if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 
- // find myDiv and insert results there
-                    //document.getElementById("testAnswer").innerHTML = xmlhttp.responseText;
-
+                    // I save here the response
                     var jsonObj = JSON.parse(xmlhttp.responseText);
-                    //document.getElementById("test1").innerHTML = jsonObj.code;
-                    //document.getElementById("test2").innerHTML = jsonObj[0].timeTableRows[0].stationShortCode;
 
+                    // First part of the array is train information, which inside has another array of trains.
                     var train = jsonObj[0];
 
+                    // In case the stations introduced are not correct
                     if (jsonObj.code === "TRAIN_NOT_FOUND") {
                         document.getElementById("errorResponse").innerHTML = jsonObj.errorMessage;
                     } else {
 
+                    // I only bring the timetable of the first train of the array
                     var arraytimeTables = jsonObj[0].timeTableRows;
 
+                    // I define the headers of the table
                     document.getElementById("chosenTrain").innerHTML = '<table class="styled-table-train"><thead><tr><th>Train Number</th><th>Departure Date</th><th>Operator</th><th>Line</th><th>Cancelled</th></tr></thead><tbody><tr><td>'+ train.trainNumber + '</td><td>'+ train.departureDate + '</td><td>'+ train.operatorShortCode + '</td><td>'+ train.commuterLineID + '</td><td>'+ train.cancelled + '</td></tr></tbody></table>';
 
                     var content, item, stationShortCode, stationUICCode, countryCode, type, trainStopping, commercialStop, commecialTrack, cancelled, scheduledTime, trainNumber;
 
+                    // I loop as to create each row for the table
                     for (i = 0; i < jsonObj[0].timeTableRows.length; i++) {
    
 
@@ -94,7 +91,10 @@ function loadJSONFile() {
                         content += item;
                     }
 
+                    // I "clean" the screen in case there is another table
                     removeStations();
+
+                    // I put the table on the screen
                     document.getElementById("tableSchedule").innerHTML = '<table class="styled-table"><thead><tr><th>Station</th><th>UI Codet</th><th>Country</th><th>Train type</th><th>Train stops at station</th><th>Commercial stop</th><th>Commercial track</th><th>Cancelled</th><th>Scheduled time</th><th>Trian number</th></tr></thead><tbody>'+content+'</tbody></table>';
         
                         }
@@ -105,6 +105,7 @@ function loadJSONFile() {
 
 function getStations(){
 
+    // This one just brings all the codes for the stations
     var url = "https://rata.digitraffic.fi/api/v1/metadata/stations";
 
     // Create AJAX object
@@ -117,7 +118,7 @@ function getStations(){
     xmlhttp.onreadystatechange = function() {
             if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 
-                     // find myDiv and insert results there
+                    // All the code is similar to previous function
 
                     var stations = JSON.parse(xmlhttp.responseText);
 
@@ -133,7 +134,7 @@ function getStations(){
                         item = '<tr><td>'+ stationName + '</td><td>'+ type + '</td><td>'+ stationShortCode + '</td></tr>';
                         content += item;
                     }
-                    // Finally we place the contents in a div
+
                     removeTableSchedule();
                     removeTableTrain();
                     document.getElementById("stationsTable").innerHTML = '<table class="styled-table"><thead><tr><th>Station name</th><th>Station type</th><th>Station short-code</th></tr></thead><tbody>'+content+'</tbody></table>';
@@ -142,6 +143,8 @@ function getStations(){
             }
 
 }
+
+//Just some functions that "remove" the tables from the screen
 
 function removeStations(){
 
